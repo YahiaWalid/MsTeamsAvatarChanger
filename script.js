fe98f8b05630da625b809bf2e4257fc121aa5513
Yahia Zaky <yahiawalidzaky@gmail.com>
2026-05-08 14:07:46 +0300

    added download button

--- script.js
+++ script.js
@@ -22,3 +22,53 @@ bgColorInput.addEventListener("input", updateAvatar);
 textColorInput.addEventListener("input", updateAvatar);
 
 updateAvatar();
+
+const downloadBtn = document.getElementById("downloadBtn");
+
+downloadBtn.addEventListener("click", downloadAvatar);
+
+function downloadAvatar() {
+
+    const canvas = document.createElement("canvas");
+
+    const size = 1024;
+
+    canvas.width = size;
+    canvas.height = size;
+
+    const ctx = canvas.getContext("2d");
+
+    // Transparent background automatically exists
+
+    // Draw circle
+    ctx.fillStyle = bgColorInput.value;
+
+    ctx.beginPath();
+
+    ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+
+    ctx.fill();
+
+    // Draw text
+    ctx.fillStyle = textColorInput.value;
+
+    ctx.font = "600 420px Segoe UI";
+
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+
+    ctx.fillText(
+        initialsInput.value.toUpperCase(),
+        size / 2,
+        size / 2 + 10
+    );
+
+    // Download image
+    const link = document.createElement("a");
+
+    link.download = "teams-avatar.png";
+
+    link.href = canvas.toDataURL("image/png");
+
+    link.click();
+}
